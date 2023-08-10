7c8f6beca7039491f99385c81600afa30d31cd99
rlawnahd <rlawnahd123@naver.com>
2023-08-10 11:19:52 +0900

    feat: 중첩 레이아웃 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,23 @@
-import './globals.css'
+import './globals.css';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      {/*
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+            <head />
+            <body>
+                <header>
+                    <nav>
+                        <h1>Demo Note</h1>
+                        <a href="/">Contract</a>
+                        <a href="/about">About</a>
+                    </nav>
+                </header>
+                {children}
+            </body>
+        </html>
+    );
 }
