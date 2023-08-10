a520601d0004859c86455c2d7570fd0867b4c623
rlawnahd <rlawnahd123@naver.com>
2023-08-10 11:06:14 +0900

    feat: not-found 페이지 추가

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from 'next/navigation';
 import React from 'react';
 
 type Props = {
@@ -7,6 +8,9 @@ type Props = {
 };
 
 export default function PantsPage({ params }: Props) {
+    if (params.slug === 'nothing') {
+        notFound();
+    }
     return <h1>{params.slug}바지 제품 설명 페이지</h1>;
 }
 
---
+++ src/app/products/not-found.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ProductNotFoundPage() {
+    return <div>해당 제품을 찾을 수 없습니다..</div>;
+}
