6a88901e160eaf206627d9240963c06095adde08
rlawnahd <rlawnahd123@naver.com>
2023-08-10 09:43:05 +0900

    next.js practice

---
+++ src/app/products/[slug]/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+type Props = {
+    params: {
+        slug: string;
+    };
+};
+
+export default function PantsPage({ params }: Props) {
+    return <h1>{params.slug}바지 제품 설명 페이지</h1>;
+}
+
+export function generateStaticParams() {
+    const products = ['skirt', 'pants'];
+    return products.map((product) => ({
+        slug: product,
+    }));
+}
