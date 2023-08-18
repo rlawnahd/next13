df59559e053c0de6887380e5ac4c56af9ae7a38f
rlawnahd <rlawnahd123@naver.com>
2023-08-18 17:45:14 +0900

    csr 추가

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,13 @@
 import { getProducts } from '@/service/products';
 import Link from 'next/link';
 import React from 'react';
-
-export const revalidate = 3; //3초마다 재검증
+import MeowArticle from '@/components/MeowArticle';
+// export const revalidate = 3; //3초마다 재검증
 //서버 파일(데이터베이스)에 있는 데이터를 가져와서 렌더링
 
 export default async function ProductsPage() {
     const products = await getProducts();
-    const res = await fetch('https://meowfacts.herokuapp.com/');
-    const data = await res.json();
-    console.log(data);
+
     return (
         <>
             <h1>제품 소개 페이지 !</h1>
@@ -20,6 +18,7 @@ export default async function ProductsPage() {
                     </li>
                 ))}
             </ul>
+            <MeowArticle />
         </>
     );
 }
