28878964afa47668c83f26faca43cf281b7d7ec4
rlawnahd <rlawnahd123@naver.com>
2023-09-01 15:11:25 +0900

    ver2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     console.log(os.hostname());
     return (
         <>
-            <div>홈페이지</div>
+            <div>홈페이지 버전 2</div>
             <Counter />
         </>
     );
